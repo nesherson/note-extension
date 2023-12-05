51cc70fd339c22b31bcd482ecabb834c64c8428c
Nešad Omeragić <nesad.omeragich27@gmail.com>
2023-12-05 23:32:43 +0100

    Add edit functionality, update styling

--- popup/test_popup.js
+++ popup/test_popup.js
@@ -1,24 +1,21 @@
-window.onload = () => {
-    setup();
+window.onload = async () => {
+    await setup();
 }
 
-function setup() {
+async function setup() {
     const popupBody = document.getElementById("popup-body");
 
-    browser.storage.local.get("textItems")
-        .then((data) => {
-            if (data?.textItems?.length > 0) {
-                data.textItems.forEach(textItem => {
-                    const textEl = createTextItemElement(textItem);
-                    popupBody.appendChild(textEl);
-                });
-            }
-
-            if (!popupBody.hasChildNodes()) {
-                toggleNoContentIndicator(true);
-            }
-
+    const data = await browser.storage.local.get("textItems");
+    if (data?.textItems?.length > 0) {
+        data.textItems.forEach(textItem => {
+            const textEl = createTextItemElement(textItem);
+            popupBody.appendChild(textEl);
         });
+    }
+
+    if (!popupBody.hasChildNodes()) {
+        toggleNoContentIndicator(true);
+    }
 
     document.getElementById("reset").onclick = resetListOnClick;
 }
@@ -35,28 +32,38 @@ function toggleNoContentIndicator(showIndicator) {
 function createTextItemElement(textItem) {
     const textListItem = document.createElement("div");
     const itemText = document.createElement("p");
+    const btnEdit = document.createElement("button");
     const btnDelete = document.createElement("button");
     const btnSend = document.createElement("button");
 
     textListItem.classList.add("text-list-item");
-    btnDelete.classList.add("item-btn-delete");
-    btnSend.classList.add("item-btn-send");
+    btnDelete.classList.add("text-list-item-btn", "btn-delete");
+    btnSend.classList.add("text-list-item-btn", "btn-send");
+    btnEdit.classList.add("text-list-item-btn", "btn-edit");
 
     textListItem.setAttribute("id", textItem.id);
-    itemText.textContent = textItem.text;
+    const truncObj = truncate(textItem.text, 180);
+    itemText.textContent = truncObj.text;
+
+    if (truncObj.isTruncated)
+        itemText.setAttribute("title", textItem.text);
+
     btnDelete.textContent = "Delete";
     btnSend.textContent = "Send";
+    btnEdit.textContent = "Edit";
 
     btnDelete.onclick = () => deleteTextItem(textItem.id);
+    btnEdit.onclick = () => editTextItem(textItem.id);
 
     textListItem.appendChild(itemText);
+    textListItem.appendChild(btnEdit);
     textListItem.appendChild(btnDelete);
     textListItem.appendChild(btnSend);
 
     return textListItem;
 }
 
-function resetListOnClick() {
+async function resetListOnClick() {
     const popupBody = document.getElementById("popup-body");
 
     while (popupBody.hasChildNodes())
@@ -64,24 +71,93 @@ function resetListOnClick() {
 
     toggleNoContentIndicator(false);
 
-    browser.storage.local.remove("textItems");
+    await browser.storage.local.remove("textItems");
 }
 
-function deleteTextItem(textItemId) {
-    browser.storage.local.get("textItems")
-        .then((data) => {
-            const newTextItems = data.textItems.filter(ti => ti.id !== textItemId);
+async function deleteTextItem(textItemId) {
+    const data = await browser.storage.local.get("textItems");
+    const newTextItems = data.textItems.filter(ti => ti.id !== textItemId);
 
-            browser.storage.local.set({ textItems: newTextItems });
+    await browser.storage.local.set({ textItems: newTextItems });
 
-            const itemToDelete = document.getElementById(textItemId);
-            itemToDelete.remove();
-        });
+    const itemToDelete = document.getElementById(textItemId);
+    itemToDelete.remove();
 }
+
+async function editTextItem(textItemId) {
+    const data = await browser.storage.local.get("textItems");
+    const textItemToEdit = data.textItems.find(ti => ti.id === textItemId);
+    const textItem = document.getElementById(textItemId);
+    const existingTextContent = textItemToEdit.text;
+
+    textItem.removeChild(textItem.firstChild);
+    textItem.removeChild(textItem.firstChild);
+
+    const textArea = document.createElement("textarea");
+    const saveButton = document.createElement("button");
+
+    textArea.classList.add("text-list-item-input");
+    textArea.value = existingTextContent;
+    saveButton.textContent = "Save";
+    saveButton.classList.add("text-list-item-btn", "btn-edit");
+    saveButton.onclick = async () => await saveEditedTextItem(textItemId);
+
+    textItem.prepend(saveButton);
+    textItem.prepend(textArea);
+
+}
+
+async function saveEditedTextItem(textItemId) {
+    const data = await browser.storage.local.get("textItems");
+    const isTruncated = false;
+    const storedTextItems = data.textItems;
+    const storedTextItemToEdit = storedTextItems.find(ti => ti.id === textItemId);
+    const textItem = document.getElementById(textItemId);
+    const input = textItem.firstChild;
+
+    storedTextItemToEdit.text = input.value;
+
+    await browser.storage.local.set({ textItems: storedTextItems });
+    textItem.removeChild(textItem.firstChild);
+    textItem.removeChild(textItem.firstChild);
+
+    const paragraph = document.createElement("p");
+    const editButton = document.createElement("button");
+
+    const truncObj = truncate(input.value, 180, isTruncated);
+    paragraph.textContent = truncObj.text;
+
+    if (truncObj.isTruncated)
+        paragraph.setAttribute("title", input.value);
+
+    editButton.textContent = "Edit";
+    editButton.classList.add("text-list-item-btn", "btn-edit");
+    editButton.onclick = () => editTextItem(textItem.id);
+
+    textItem.prepend(editButton);
+    textItem.prepend(paragraph);
+}
+
+function truncate(str, n) {
+    const trunc = {
+        text: str,
+        isTruncated: false
+    };
+
+    if (str.length > n) {
+        trunc.isTruncated = true;
+        trunc.text = str.slice(0, n - 1) + '...';
+
+        return trunc
+    }
+
+    return trunc;
+};
 
 /*
 <div class="text-list-item">
-      <p class="item-text"></p>
+      <p></p>
+      <button class="item-btn-edit">Edit</button>
       <button class="item-btn-delete">Delete</button>
       <button class="item-btn-send">Send</button>
     </div>
--- script.js
+++ script.js
@@ -1,90 +1,94 @@
 document.title = "Test";
 
 browser.runtime.onMessage.addListener((request) => {
-    if (request.command === "save-text") {
+    if (request.command === "save-highlighted-text") {
         const selectedText = getSelectedText();
-        saveTextToStorage(selectedText);
+
+        if (selectedText !== "")
+            saveTextToStorage(selectedText);
     }
 
     return Promise.resolve({ response: "Hi from content script" });
 });
 
 function getSelectedText() {
-    const selection = window.getSelection();
-    if (selection.anchorNode.textContent === "") return;
+    try {
+        const selection = window.getSelection();
+        if (selection.anchorNode.textContent === "") return "";
 
-    let selectedText = "";
-    let anchorOffset = selection.anchorOffset;
-    let focusOffset = selection.focusOffset;
+        let selectedText = "";
+        let anchorOffset = selection.anchorOffset;
+        let focusOffset = selection.focusOffset;
 
-    const anchorNodeTextContent = selection.anchorNode.textContent;
-    const focusNodeTextContent = selection.focusNode.textContent;
+        const anchorNodeTextContent = selection.anchorNode.textContent;
+        const focusNodeTextContent = selection.focusNode.textContent;
 
-    // console.log("getSelectedText/selection -> ", selection);
+        // console.log("getSelectedText/selection -> ", selection);
 
-    if (
-        selection.anchorNode.nextSibling === selection.focusNode.parentElement
-    ) {
-        const textLengthTillNextNode =
-            anchorNodeTextContent.length - anchorOffset;
-        const allText = anchorNodeTextContent.concat(focusNodeTextContent);
-
-        selectedText = allText.slice(
-            anchorOffset,
-            anchorOffset + textLengthTillNextNode + focusOffset
-        );
-    } else {
-        if (selection.anchorOffset > selection.focusOffset &&
-            selection.anchorNode.parentElement === selection.focusNode.parentElement) {
+        if (
+            selection.anchorNode.nextSibling === selection.focusNode.parentElement
+        ) {
             const textLengthTillNextNode =
                 anchorNodeTextContent.length - anchorOffset;
-            const anchorNodeWithNextSiblingTextContent = anchorNodeTextContent.concat(
-                selection.anchorNode.nextSibling.textContent
-            );
-
-            const allText = anchorNodeWithNextSiblingTextContent.concat(selection.focusNode.textContent.slice(0, focusOffset));
+            const allText = anchorNodeTextContent.concat(focusNodeTextContent);
 
             selectedText = allText.slice(
                 anchorOffset,
-                anchorOffset +
-                textLengthTillNextNode +
-                selection.anchorNode.nextSibling.textContent.length +
-                focusOffset
+                anchorOffset + textLengthTillNextNode + focusOffset
             );
+        } else {
+            if (selection.anchorOffset > selection.focusOffset &&
+                selection.anchorNode.parentElement === selection.focusNode.parentElement) {
+                const textLengthTillNextNode =
+                    anchorNodeTextContent.length - anchorOffset;
+                const anchorNodeWithNextSiblingTextContent = anchorNodeTextContent.concat(
+                    selection.anchorNode.nextSibling.textContent
+                );
 
-            // console.log("getSelectedText/selectedText -> ", selectedText);
+                const allText = anchorNodeWithNextSiblingTextContent.concat(selection.focusNode.textContent.slice(0, focusOffset));
 
-            return selectedText;
+                selectedText = allText.slice(
+                    anchorOffset,
+                    anchorOffset +
+                    textLengthTillNextNode +
+                    selection.anchorNode.nextSibling.textContent.length +
+                    focusOffset
+                );
+
+                // console.log("getSelectedText/selectedText -> ", selectedText);
+
+                return selectedText;
+            }
+            else if (selection.anchorNode.parentElement === selection.focusNode.parentElement) {
+                selectedText = anchorNodeTextContent.slice(selection.anchorOffset, selection.focusOffset);
+                // console.log("getSelectedText/selectedText -> ", selectedText);
+                return selectedText;
+            }
         }
-        else if (selection.anchorNode.parentElement === selection.focusNode.parentElement) {
-            selectedText = anchorNodeTextContent.slice(selection.anchorOffset, selection.focusOffset);
-            // console.log("getSelectedText/selectedText -> ", selectedText);
-            return selectedText;
-        }
+
+        // console.log("getSelectedText/selectedText -> ", selectedText);
+
+        return selectedText;
     }
-
-    // console.log("getSelectedText/selectedText -> ", selectedText);
-
-    return selectedText;
+    catch (error) {
+        return "";
+    }
 }
 
-function saveTextToStorage(text) {
+async function saveTextToStorage(text) {
     let existingTextItems = [];
-
     const newTextItem = createTextItem(text);
 
-    browser.storage.local.get("textItems")
-        .then(data => {
-            if (data?.textItems?.length > 0) {
-                existingTextItems = data.textItems.slice();
-                existingTextItems.push(newTextItem);
-                browser.storage.local.set({ textItems: existingTextItems });
-            }
-            else {
-                existingTextItems.push(newTextItem);
-                browser.storage.local.set({ textItems: existingTextItems });
-            }
-        });
+    const data = await browser.storage.local.get("textItems");
+    if (data?.textItems?.length > 0) {
+        existingTextItems = data.textItems.slice();
+        existingTextItems.push(newTextItem);
+        browser.storage.local.set({ textItems: existingTextItems });
+    }
+    else {
+        existingTextItems.push(newTextItem);
+        browser.storage.local.set({ textItems: existingTextItems });
+    }
 }
 
 function createTextItem(text) {
