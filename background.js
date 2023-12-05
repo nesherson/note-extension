421ea9de8aeca13ce7a57e975e010429b6bb40a9
Nešad Omeragić <nesad.omeragich27@gmail.com>
2023-12-05 23:31:12 +0100

    Add command for saving text

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
 browser.contextMenus.create(
     {
-        id: "save-text",
+        id: "save-highlighted-text",
         title: "Save highlighted text",
         contexts: ["selection"],
     },
-    () => { },
+    onCreate,
 );
 
-browser.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "save-text") {
-        browser.tabs.sendMessage(tab.id, { command: "save-text"});
+browser.contextMenus.onClicked.addListener(async (info, tab) => {
+    if (info.menuItemId === "save-highlighted-text") {
+        await browser.tabs.sendMessage(tab.id, { command: "save-highlighted-text" });
     }
 });
+
+browser.commands.onCommand.addListener(async (command) => {
+    if (command === "save-highlighted-text") {
+        const tabs = await browser.tabs.query({ active: true, windowId: browser.windows.WINDOW_ID_CURRENT });
+        await browser.tabs.sendMessage(tabs[0].id, { command: "save-highlighted-text" });
+    }
+});
+
+function onCreate() {
+    console.log("Context menu item created");
+}
 
